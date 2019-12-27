d680639fcd55d06c055579fc062429fb702ebd4b
jp117 <john.paradise117@gmail.com>
2019-12-27 09:51:41 -0500

    completed 009 - allLongestStrings

---
+++ Arcade/Intro/009 - allLongestStrings.py
@@ -0,0 +1,45 @@
+'''
+https://app.codesignal.com/arcade/intro/level-3/fzsCQGYbxaEcTr2bL
+
+Given an array of strings, return another array containing all of its longest strings.
+
+Example
+
+For inputArray = ["aba", "aa", "ad", "vcd", "aba"], the output should be
+allLongestStrings(inputArray) = ["aba", "vcd", "aba"].
+
+Input/Output
+
+[execution time limit] 4 seconds (py3)
+
+[input] array.string inputArray
+
+A non-empty array.
+
+Guaranteed constraints:
+1 ≤ inputArray.length ≤ 10,
+1 ≤ inputArray[i].length ≤ 10.
+
+[output] array.string
+
+Array of the longest strings, stored in the same order as in the inputArray.
+'''
+
+def checkLength(inputArray):
+    i = 0
+    length = 0
+    for i in range(len(inputArray)):
+        x = len(inputArray[i])
+        if x > length:
+            length = x
+    return length
+
+def allLongestStrings(inputArray):
+    i = 0
+    length = checkLength(inputArray)
+    print(length)
+    output = []
+    for i in range(len(inputArray)):
+        if len(inputArray[i]) == length:
+            output.append(inputArray[i])
+    return output
