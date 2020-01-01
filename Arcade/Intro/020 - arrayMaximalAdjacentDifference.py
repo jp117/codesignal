6300a98f27bcc071fa71d08cfdf7e0a2b2acbdab
jp117 <john.paradise117@gmail.com>
2020-01-01 10:48:10 -0500

    finished challenege 20

--- Arcade/Intro/020 - arrayMaximalAdjacentDifference.py
+++ Arcade/Intro/020 - arrayMaximalAdjacentDifference.py
@@ -1,15 +1,22 @@
 def arrayMaximalAdjacentDifference(inputArray):
-    first = -16
-    place = 0
-    i = 0
-    for i in range(len(inputArray)):
-        if abs(inputArray[i]) > first:
-            first = abs(inputArray[i])
-            place = i
-    if place > 0:
-        if abs(inputArray[place]) - abs(inputArray[place-1]) > abs(inputArray[place]) - abs(inputArray[place+1]):
-            return abs(inputArray[place]) - abs(inputArray[place-1])
-    return abs(inputArray[place]) - abs(inputArray[place+1])
+    i = 0;
+    difference = -15
+    for i in range(len(inputArray)-1):
+        if i == 0:
+            if abs(inputArray[i] - inputArray[i+1]) > difference:
+                difference = abs(inputArray[i] - inputArray[i+1])
+        elif (i < (len(inputArray)-1)):
+            if abs(inputArray[i] - inputArray[i-1]) > difference:
+                difference = abs(inputArray[i] - inputArray[i-1])
+            if abs(inputArray[i] - inputArray[i+1]) > difference:
+                difference = abs(inputArray[i] - inputArray[i+1])
+        else:
+            if abs(inputArray[i] - inputArray[i-1]) > difference:
+                difference = abs(inputArray[i] - inputArray[i-1])
+    return difference
+
 
 if __name__ == "__main__":
     print(arrayMaximalAdjacentDifference([2, 4, 1, 0]))
+    print(arrayMaximalAdjacentDifference([10, 11, 13]))
+    print(arrayMaximalAdjacentDifference([-1, 1, -3, -4]))
